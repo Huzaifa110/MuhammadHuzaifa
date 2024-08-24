f71d49542fbb0cef76605659b244655ad1c8cac0
Muhammad Huzaifa <huzaifazafar750@gmail.com>
2024-08-24 16:41:10 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+
 export default defineConfig({
-  base: '/MuhammadHuzaifa/',
   plugins: [react()],
+  base: '/MuhammadHuzaifa/',
   assetsInclude: ['**/*.png', '**/*.PNG', '**/*.mp4']
 });
